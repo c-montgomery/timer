3e546f77ca78b9849f3c7bd85656ecb4d9c99ccb
Collin M <craym87@gmail.com>
2021-06-09 00:55:53 -0700

    added comments to functions

--- timer.py
+++ timer.py
@@ -9,6 +9,7 @@ class Egg_timer:
         self.minutes = 1
         self.elapsed_seconds = 0
 
+    '''Starts counting time, calls play_alarm function'''
     def start_timer(self):
         seconds_set = int(self.minutes)*60
         start_timer = time.time()
@@ -16,7 +17,7 @@ class Egg_timer:
             self.elapsed_seconds = round(time.time() - start_timer, 2)
             print(round(self.elapsed_seconds, 2))
         self.play_alarm()
-
+    '''initializes audio player, plays audio until space is pressed'''
     def play_alarm(self):
         mixer.init()
         alarm_audio = mixer.music.load("C:/Users/craym/Desktop/alarm2.wav")
