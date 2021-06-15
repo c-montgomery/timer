220b03320ed3cccf88437536096a37033f91bc6b
Collin M <craym87@gmail.com>
2021-06-14 19:18:05 -0700

    Added label for displaying elapsed time

--- timergui.py
+++ timergui.py
@@ -2,20 +2,27 @@ import tkinter as tk
 from tkinter.ttk import Combobox
 from timer import EggTimer
 from pygame import mixer
-
+from tkinter import *
 class Window():
     def __init__(self, arg):
         self.arg = tk.Tk()
         self.arg.title('Egg timer')
+        '''Creates display for timer'''
+        self.label = tk.Label(self.arg, text = 'Fuck')
+        self.label.pack()
 
         # self.button1 needs to change command to start_timer(5) after testing
         self.button1 = tk.Button( self.arg, text = '5 minutos', command=lambda: self.start_timer('timer1', 5))
         self.button1.pack()
 
+        self.button1 = tk.Button( self.arg, text = '15 minutos', command=lambda: self.start_timer('timer1', 15))
+        self.button1.pack()
+
+
         '''listen for keypress is bound to main window'''
         self.arg.bind('<Key>', self.key_press)
         self.arg.mainloop()
-        
+
     '''creates EggTimer and starts counting'''
     def start_timer(self, name, minutes):
         name = EggTimer()
