02804a1c9ae73b7374e63dd7bc7076d75ba322ce
Collin M <craym87@gmail.com>
2021-06-16 10:15:17 -0700

    Display functioning, added icon and title

--- timergui.py
+++ timergui.py
@@ -11,34 +11,39 @@ class Window():
         
         '''Creates display for timer'''
         self.name = tk.Tk()
+        self.name.iconbitmap('favicon.ico')
+        self.name.title('Timer')
+        self.egg = EggTimer()
+        self.start_time = 0
+        self.current_time = 0
+        self.time_elapsed = 0
+        self.is_running = False
+
+        self.label = tk.Label(self.name, text = self.time_elapsed)
+        self.label.pack()
         # self.button1 needs to change command to start_timer(5) after testing
         self.button1 = tk.Button( self.name, text = '5 minutos', command=lambda: self.start_timer('timer1', 1))
         self.button1.pack()
 
         self.button1 = tk.Button( self.name, text = '15 minutos', command=lambda: self.start_timer('timer1', 15))
         self.button1.pack()
-        self.egg = EggTimer()
-        self.start_time = time.time()
-        self.current_time = time.time()
-        self.time_elapsed = 0
+        
 
-        self.label = tk.Label(self.name, text = self.time_elapsed)
-        self.label.pack()
+        
         
         self.name.mainloop()
 
     '''creates EggTimer and starts counting'''
     def start_timer(self, name, minutes):
+        self.start_time = time.time()
         self.current_time = time.time()
-        self.update_timer(self.current_time)
-        
-        self.time_elapsed = round(self.current_time ,1) - round(self.start_time, 1)
+        self.update_timer()
         
     '''Updates countdown display'''
-    def update_timer(self, time):
-        
-        self.label.config(text = time)
-        self.name.after(1000, self.update_timer())
+    def update_timer(self):
+        self.time_elapsed = time.time() - self.start_time
+        self.label.config(text = round(self.time_elapsed, 1))
+        self.name.after(100, self.update_timer)
 
     '''listens for keypress and stops alarm'''    
     def key_press(self, event):
